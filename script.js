83b4dad0e70c7d1106f9664fee9b5cd15fddd779
PALAKURI KUMARA SWAMY <76578469+Kumarswamy-palakuri@users.noreply.github.com>
2023-07-25 10:32:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function myF1()
+{
+    var num1, num2, sum;
+num1 = parseInt(document.getElementById("num1").value);
+num2 = parseInt(document.getElementById("num2").value);
+   sum = num1 + num2;
+document.getElementById("num3").value = sum;
+}function myF2()
+{
+    var num1, num2, sum;
+num1 = parseInt(document.getElementById("num1").value);
+num2 = parseInt(document.getElementById("num2").value);
+   sum = num1 - num2;
+document.getElementById("num3").value = sum;
+}function myF3()
+{
+    var num1, num2, sum;
+num1 = parseInt(document.getElementById("num1").value);
+num2 = parseInt(document.getElementById("num2").value);
+   sum = num1 * num2;
+document.getElementById("num3").value = sum;
+}
+function myF4()
+{
+    var num1, num2, sum;
+num1 = parseInt(document.getElementById("num1").value);
+num2 = parseInt(document.getElementById("num2").value);
+   sum = num1 / num2;
+document.getElementById("num3").value = sum;
+}
