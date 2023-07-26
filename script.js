5d6581cc087917179c4b7fe7cd3992d1b8ec4f5a
PALAKURI KUMARA SWAMY <19r21a0498@mlrinstitutions.ac.in>
2023-07-26 14:43:09 +0530

    update js

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
+let num1, num2, sum;
+num1 = parseInt(document.getElementById("num1"));
+num2 = parseInt(document.getElementById("num2"));
 function myF1()
 {
-    var num1, num2, sum;
-num1 = parseInt(document.getElementById("num1").value);
-num2 = parseInt(document.getElementById("num2").value);
+    let num1, num2, sum;
+    num1 = parseInt(document.getElementById("num1").value);
+    num2 = parseInt(document.getElementById("num2").value);
    sum = num1 + num2;
 document.getElementById("num3").value = sum;
-}function myF2()
+}
+function myF2()
 {
     var num1, num2, sum;
-num1 = parseInt(document.getElementById("num1").value);
-num2 = parseInt(document.getElementById("num2").value);
+    num1 = parseInt(document.getElementById("num1").value);
+    num2 = parseInt(document.getElementById("num2").value);
    sum = num1 - num2;
 document.getElementById("num3").value = sum;
-}function myF3()
+}
+function myF3()
 {
     var num1, num2, sum;
-num1 = parseInt(document.getElementById("num1").value);
-num2 = parseInt(document.getElementById("num2").value);
+    num1 = parseInt(document.getElementById("num1").value);
+    num2 = parseInt(document.getElementById("num2").value);
    sum = num1 * num2;
 document.getElementById("num3").value = sum;
 }
 function myF4()
 {
     var num1, num2, sum;
-num1 = parseInt(document.getElementById("num1").value);
-num2 = parseInt(document.getElementById("num2").value);
+    num1 = parseInt(document.getElementById("num1").value);
+    num2 = parseInt(document.getElementById("num2").value);
    sum = num1 / num2;
 document.getElementById("num3").value = sum;
 }
